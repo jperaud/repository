ce33aa88f4cd8c03a425149fbbe617ee14bbb55a
jperaud <jperaud@users.noreply.github.com>
2015-06-02 21:29:50 +0200

    test

---
+++ src/main/java/test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Julie
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
